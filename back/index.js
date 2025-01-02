65aebd4305bb765ab16b40e7db22798edfb76d78
Pauline-Devavry <devavrypauline@gmail.com>
2025-01-02 16:36:26 +0100

    ticket 3 - création du serveur et connexion avec la BDD

---
+++ back/index.js
@@ -0,0 +1,26 @@
+import "dotenv/config"; // Pour charger le fichier .env
+import express from 'express';
+import { sequelize } from './src/client.js'; // Importation de la fonction sequelize
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connexion à la base de données
+sequelize().authenticate()  // Vérification de la connexion
+  .then(() => {
+    console.log("Connexion à la base de données réussie.");
+  })
+  .catch((err) => {
+    console.error("Impossible de se connecter à la base de données :", err);
+  });
+
+// Route de base
+app.get('/', (req, res) => {
+  res.send('Hello les pattounes!');
+});
+
+// Lancer le serveur
+app.listen(port, () => {
+  console.log(`🚀 API Les Pattounes demarrée à l'adresse : http://localhost:${port}`);
+});
+
