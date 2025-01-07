564d607840d3dae5dfee23c5dcb41cdd397a1101
Pauline-Devavry <devavrypauline@gmail.com>
2025-01-07 16:34:12 +0100

    Ticket 2 - Configuration of Tailwind CSS

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,13 @@
+import './index.css'; // Importation du CSS avec Tailwind
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Bienvenue sur Les Pattounes !</h1>
+    <h1 className="bg-blue-500 text-white p-10">Bienvenue sur Les Pattounes !</h1>
   </React.StrictMode>
 );
+
+
