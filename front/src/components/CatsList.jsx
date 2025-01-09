4c371bc7291afe97d68708067065c4fceb18e8e1
Pauline-Devavry <devavrypauline@gmail.com>
2025-01-09 16:01:30 +0100

    Rename chats in english

--- front/src/components/CatsList.jsx
+++ front/src/components/CatsList.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
 function CatsList() {
-  const [chats, setChats] = useState([])
+  const [cats, setCats] = useState([])
 
   useEffect(() => {
     fetch('/api/cats')
       .then((response) => response.json()) // Récupère la réponse sous forme de JSON
-      .then((data) => setChats(data)) // Stocke les chats dans l'état
+      .then((data) => setCats(data)) // Stocke les chats dans l'état
       .catch((error) =>
         console.error('Erreur lors de la récupération des chats:', error),
       )
@@ -16,7 +16,7 @@ function CatsList() {
     <div>
       <h2>Liste des Chats</h2>
        <ul>
-        {chats.map((cat) => (
+        {cats.map((cat) => (
           <li key={cat.id}>
             {cat.name} - {cat.age} ans
             {cat.description}
