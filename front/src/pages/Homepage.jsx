ac6982f41025dc9b1f9f25ad9ed75be214215499
Pauline-Devavry <devavrypauline@gmail.com>
2025-01-09 15:59:06 +0100

    Add CatsList component on homepage

--- front/src/pages/Homepage.jsx
+++ front/src/pages/Homepage.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import CatsList from '../components/CatsList';
 
 function Homepage() {
 
   return (
-    <h1>Homepage</h1>
+    <div>
+      <h1>Homepage</h1>
+      <CatsList />
+    </div>
   )
 }
 
