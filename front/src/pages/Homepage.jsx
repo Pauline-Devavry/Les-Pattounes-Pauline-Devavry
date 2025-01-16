9aab9e82fc4141af0f0b13439c177628e0b212b3
Pauline-Devavry <devavrypauline@gmail.com>
2025-01-16 13:36:19 +0100

    Ticket 18 - Create container for each section to allow section with full width

--- front/src/pages/Homepage.jsx
+++ front/src/pages/Homepage.jsx
@@ -10,17 +10,21 @@ function Homepage() {
       {/*  HEADER */}
       <Header />
 
+      {/* TITRE H1 */}
       <div className="container mx-auto px-4">
-        {/*   TITRE H1 */}
         <div className="text-h1TextColor font-bold text-5xl font-montserrat text-center py-14">
           <h1>Les Pattounes du Coeur</h1>
         </div>
+      </div>
 
-        {/* BLOC QUI SOMMES NOUS */}
+      {/* BLOC QUI SOMMES NOUS */}
+      <div className="container mx-auto px-4">
         <section className="m-0 p-0">
           <div className="flex flex-col md:flex-row">
             <div className="flex flex-col justify-center w-full items-center text-center md:w-1/2 md:pr-4 md:items-start md:text-left">
-              <h2 className="text-h2TextColor font-bold text-lg font-montserrat mb-6">Qui sommes-nous ?</h2>
+              <h2 className="text-h2TextColor font-bold text-lg font-montserrat mb-6">
+                Qui sommes-nous ?
+              </h2>
               <p className="mb-7">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -60,22 +64,26 @@ function Homepage() {
             </div>
           </div>
         </section>
+      </div>
 
-        {/* BLOC NOS CHATS A L'ADOPTION */}
+      {/* BLOC NOS CHATS A L'ADOPTION */}
+      <div className="container mx-auto px-4">
         <h1 className="text-h2TextColor text-center font-bold text-lg font-montserrat py-6">
           Nos chats à l'adoption
         </h1>
         <CatsList limit={6} />
+      </div>
 
-        {/* BLOC QUELQUES CHIFFRES */}
-        <section className="py-11 border-2 border-primaryColor bg-transparent">
-          <div className="font-montserrat text-center font-bold text-h2TextColor">
-            <p>+ de 120 adoptions</p>
-            <p>+ de 80 bénévoles</p>
-          </div>
-        </section>
+      {/* BLOC QUELQUES CHIFFRES */}
+      <section className="py-11 border-2 border-primaryColor bg-transparent">
+        <div className="font-montserrat text-center font-bold text-h2TextColor">
+          <p>+ de 120 adoptions</p>
+          <p>+ de 80 bénévoles</p>
+        </div>
+      </section>
 
-        {/* BLOC POURQUOI ADOPTER */}
+      {/* BLOC POURQUOI ADOPTER */}
+      <div className="container mx-auto px-4">
         <section className="py-6">
           <h2 className="text-h2TextColor font-bold text-lg font-montserrat">
             Pourquoi adopter ?
