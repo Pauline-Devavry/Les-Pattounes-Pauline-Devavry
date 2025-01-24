b8543868ac8788e4b72fb20163e16026e598d67f
Pauline-Devavry <devavrypauline@gmail.com>
2025-01-24 11:06:45 +0100

    Ticket 06 - add feature send email with nodemailer

--- back/src/router/contact.router.js
+++ back/src/router/contact.router.js
@@ -1,16 +1,46 @@
+import "dotenv/config"; 
 import { Router } from "express";
+import nodemailer from 'nodemailer';
 
 const router = Router();
 
+
 // Route POST pour le formulaire de contact
-router.post('/api/contact', (req, res) => {
-    const { name, email, subject, message } = req.body;
-  
-    // Logique pour traiter les données du formulaire de contact
-    console.log('Données reçues :', { name, email, subject, message });
-  
-    // Réponse au client
-    res.status(200).json({ message: 'Message envoyé avec succès !' });
-  });
+router.post('/api/contact', async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+
+  try {
+    // Création du transporteur pour l'envoi de mails
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    // Config du mail
+    const mailOptions = {
+      from: `"${name}" <${email}>"`,
+      replyTo: email,
+      to: "projetlespattounes@gmail.com",
+      subject: subject || 'Contact via le formulaire',
+      text: message,
+    };
+
+    // Envoi de l'email
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: 'Email envoyé avec succès !' });
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi de l\'email :', error);
+    res.status(500).json({ message: 'Erreur lors de l\'envoi de l\'email.' });
+  }
+});
+
 
 export default router
