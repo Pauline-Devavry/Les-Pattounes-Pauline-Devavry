14668efbb15b5e1dbf9ba3e2c7fc56ed1bdeb74c
Pauline-Devavry <devavrypauline@gmail.com>
2025-02-11 17:03:02 +0100

    Ticket 05 - design CatDetailsPage in progress

--- front/src/components/CatDetailsCard.jsx
+++ front/src/components/CatDetailsCard.jsx
@@ -1,15 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import imageCardCat from '../images/img-chat.jpg'
+import Button from './Button'
 
 function CatDetailsCard({ cat }) {
 
   return (
-    <div className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg">
-      <img src={imageCardCat} alt="image chat" className="w-full h-64 object-cover rounded-md" />
-      <h2 className="text-2xl font-bold mt-4">{cat.name}</h2>
-      <p className="text-gray-700 mt-2">Âge : {cat.age} ans</p>
-      <p className="text-gray-600 mt-2">{cat.description}</p>
+<div className="flex flex-col bg-white shadow-xl border border-gray-300 rounded-lg my-6 w-[400px] h-[550px]">
+  <div className="m-2.5 overflow-hidden rounded-md h-96 flex justify-center items-center bg-blue-50">
+    <img className="w-full h-full object-cover" src={imageCardCat} alt="photo chat" />
+  </div>
+  <div className="p-6 text-center flex flex-col justify-between h-full">
+    <h1 className="mb-2 text-3xl font-bold text-blue-600">
+      {cat.name}
+    </h1>
+    <p className="text-sm font-semibold text-gray-600 uppercase">
+      Âge : {cat.age} ans
+    </p>
+    <p className="text-base text-gray-700 mt-4 font-light leading-relaxed">
+      {cat.description}
+    </p>
+    <div className="flex justify-center p-6 pt-2 gap-7 mt-auto">
+    <Button text="Nous contacter" to="/contact" />
     </div>
+  </div>
+</div>
+
+
+
+
   )
 }
 
--- front/src/pages/CatDetailsPage.jsx
+++ front/src/pages/CatDetailsPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import CatDetails from '../components/CatDetails';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function CatDetailsPage() {
   return (
     <div>
+        < Header/>
       < CatDetails/>
+      < Footer/>
     </div>
   );
 }
