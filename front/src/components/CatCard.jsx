a7724e1796332a95d670f097f3113ac0ea44eaff
Pauline-Devavry <devavrypauline@gmail.com>
2025-02-17 17:21:19 +0100

    Ticket 05 - modif link CatCard

--- front/src/components/CatCard.jsx
+++ front/src/components/CatCard.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 function CatCard({ id, name, age }) {
 
   return (
+    
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 transform hover:scale-110 transition duration-200 ease-in-out">
+      <Link to={`/chat/${id}`} className="cursor-pointer">
       <img className='w-full h-72 object-cover mx-auto border-[8px] border-cardColor rounded-xl' src={imageCardCat} alt="image chat" />
       <h3 className=" text-center text-red-950 font-bold">
         {name} - {age} ans
       </h3>
-      <Link to={`/chat/${id}`} className="text-blue-500 hover:underline">
-        Voir plus
       </Link>
     </div>
   )
